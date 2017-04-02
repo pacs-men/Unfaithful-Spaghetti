0d43d1d81c272eddc22aa3ef2c905e8f63f82dfd
MenaceNas <missancassa@gmail.com>
2017-04-02 23:33:02 +0200

    Affichages des persos sur la carte

---
+++ Jeu Pac s Men/Carte/AffichagePersos.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+import sys
+sys.path.append("C:\Users\nassim\Documents\GitHub\Unfaithful-Spaghetti\Jeu Pac s Men\combat")
+import combat
+
+combat.perso
