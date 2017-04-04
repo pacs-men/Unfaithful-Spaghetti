1d610f34dd678d63ae9778c425c4ac28c8d3b3b8
loul213 <canard.meteo@free.fr>
2017-04-04 16:02:03 +0200

    Update affichagecarte.py

--- Jeu Pac s Men/Carte/affichagecarte.py
+++ Jeu Pac s Men/Carte/affichagecarte.py
@@ -26,7 +26,7 @@ def affichercarte(x0,y0):
     for x in range(D):
         for y in range(D):
             fenetre.blit(mape.get_image_case(x+x0, y+y0),(x*64, y*64))
-perso = combat.AssissinsMagique.img
+perso = combat.AssassinsMagique.img
 position_perso = perso.get_rect()
 fenetre.blit(perso, position_perso)
 
