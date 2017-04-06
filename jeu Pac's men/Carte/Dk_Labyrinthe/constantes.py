1c01bc3c5818f2673dc4f59792e2de4b0566e5ed
loul213 <canard.meteo@free.fr>
2017-04-06 10:27:58 +0200

    update

---
+++ jeu Pac's men/Carte/Dk_Labyrinthe/constantes.py
@@ -0,0 +1,17 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "DK Labyrinthe"
+image_icone = "images/dk_droite.png"
+
+#Listes des images du jeu
+image_accueil = "images/accueil.png"
+image_fond = "images/fond.jpg"
+image_mur = "images/mur.png"
+image_depart = "images/depart.png"
+image_arrivee = "images/arrivee.png"
--- jeu Pac's men/combat/popoactif.py
+++ jeu Pac's men/combat/popoactif.py
@@ -7,13 +7,23 @@ Created on Fri Mar 24 10:20:28 2017
 
 def popo_actif(self):
     potion=self.action
-    if potion == "vie1": self.pv += 50
-    
-    if potion == "vie2": self.pv += 100
-    
-    if potion == "vie3": self.pv += 250
-    
-    if potion == "force1": self.atk += 50 
+    if potion == "vie1":
+        self.pv += 50
+        if self.pv > self.pv_max:
+            self.pv = self.pv_max
+            
+    if potion == "vie2":
+        self.pv += 100
+        if self.pv > self.pv_max:
+            self.pv = self.pv_max
+            
+    if potion == "vie3":
+        self.pv += 250
+        if self.pv > self.pv_max:
+            self.pv = self.pv_max
+            
+    if potion == "force1":
+        self.atk += 50 
     self.mag += 50  
     self.force1 =2
     self.force1fois +=1
@@ -53,6 +63,7 @@ def popo_actif(self):
     
     if potion == "critique3": self.crit += 15
     self.critique3 =2 #"tour"
+    self.critique3fois +=1    
     
     if potion == "vitesse": self.vit += 100
     self.vitesse =1 #"combat"
@@ -64,28 +75,88 @@ def popo_actif(self):
     
     
 def popo_def(self):
-        if self.effet==True:
-            if self.brulure > 0:
-                self.pv -= 5
-                self.brulure -= 1
-            elif self.saignement > 0:
-                self.pv -= 10
-                self.saignement -= 1
+        if self.precisionfois > 0:
+            if self.precision == 0:
+                self.prec -= 10
+                self.precisionfois -= 1
             else:
-                self.effet=False
+                self.precison -=1
+
+        if self.vitessefois > 0:
+            if self.vitesse == 0:
+                self.vit -= 10
+                self.vitessefois -= 1
+            else:
+                self.vitesse -=1
+
+        if self.critique3fois > 0:
+            if self.critique3 == 0:
+                self.crit -= 15
+                self.critique3fois -= 1
+            else:
+                self.critique3 -=1
     
+        if self.critique2fois > 0:
+            if self.critique2 == 0:
+                self.crit -= 10
+                self.critique2fois -= 1
+            else:
+                self.critique2 -=1    
     
+        if self.critique1fois > 0:
+            if self.critique1 == 0:
+                self.crit -= 5
+                self.critique1fois -= 1
+            else:
+                self.critique1 -=1     
+
+        if self.armure1fois > 0:
+            if self.armure1 == 0:
+                self.defend -= 5
+                self.res -=5
+                self.armure1fois -= 1
+            else:
+                self.armure1 -=1  
+
+        if self.armure2fois > 0:
+            if self.armure2 == 0:
+                self.defend -= 5
+                self.res -=5
+                self.armure2fois -= 1
+            else:
+                self.armure2 -=1      
+
+        if self.armure3fois > 0:
+            if self.armure3 == 0:
+                self.defend -= 5
+                self.res -=5
+                self.armure3fois -= 1
+            else:
+                self.armure3 -=1     
     
+        if self.force3fois > 0:
+            if self.force3 == 0:
+                self.atk -= 150
+                self.mag -= 150
+                self.force3fois -= 1
+            else:
+                self.force3 -=1       
+     
+        if self.force2fois > 0:
+            if self.force2 == 0:
+                self.atk -= 100
+                self.mag -= 100
+                self.force2fois -= 1
+            else:
+                self.force2 -=1      
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        if self.force1fois > 0:
+            if self.force1 == 0:
+                self.atk -= 50
+                self.mag -= 50
+                self.force1fois -= 1
+            else:
+                self.force1 -=1      
     
     
     
